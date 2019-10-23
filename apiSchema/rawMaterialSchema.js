d816d8524ad4d5a4e65d780c70c35d7279e97376
suyog <suyog15122@gmail.com>
2019-10-23 11:28:04 +0530

    adding changes to suyog

--- apiSchema/rawMaterialSchema.js
+++ apiSchema/rawMaterialSchema.js
@@ -10,3 +10,9 @@ module.exports.getAllRawMaterialSchema = Joi.object().keys({
     skip: Joi.string(),
     limit: Joi.string()
 });
+
+module.exports.updateRawMaterialSchema = Joi.object().keys({
+    name: Joi.string(),
+    price: Joi.number(),
+    supplier: Joi.string()
+});
